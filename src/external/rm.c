027b98741bac704799ca0c8877efaa23f883fc83
Shwetank Shrey <shwetankshrey@gmail.com>
2018-01-25 14:13:43 +0530

    rm added

---
+++ src/external/rm.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+
+void main(int argc, char **argv) {
+    int i = 1;
+    int iflag = 0;
+    int dflag = 0;
+    for( ; argv[i] != NULL && argv[i][0] == '-' ; i++) {
+        if(strcmp(argv[i], "-i") == 0) {
+            iflag = 1;
+        }
+        else if(strcmp(argv[i], "-d") == 0) {
+            dflag = 1;
+        }
+        else {
+            printf("Syntax error. Refer to readme.\n");
+            return;
+        }
+    }
+    if(argv[i] == NULL) {
+        printf("Not enough arguments\n");
+        return;
+    }
+    for( ; argv[i] != NULL ; i++) {
+        if (argv[i+1] == NULL) {
+            argv[i][strlen(argv[i])-1] == 0;
+        }
+        if(iflag) {
+            printf("Are you sure you want to remove %s? ", argv[i]);
+            char str[50];
+            gets(str);
+            if(strcasecmp(str, "y") != 0) {
+                return;
+            }
+        }
+        DIR *dir = opendir(argv[i]);
+        if(dir == NULL) {
+            FILE *fr = fopen(argv[i], "r");
+            if(fr == NULL) {
+                printf("File does not exist.\n");
+                return;
+            }
+            remove(argv[i]);
+            fclose(fr);
+            continue;
+        }
+        if(dflag == 0) {
+            printf("Cannot perform rm on directories.\n");
+            closedir(dir);
+            return;
+        }
+        if(rmdir(argv[i]) == 0) {
+            closedir(dir);
+            continue;
+        }
+        printf("Non empty directory. Cannot perform rm.\n");
+        closedir(dir);
+        return;
+    }
+}
